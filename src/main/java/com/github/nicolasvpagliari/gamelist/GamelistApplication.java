3012252e0d97f967096be35e2f72223add8ff890
NicolasVPagliari <nicolasvpagliari@gmail.com>
2025-08-19 20:15:05 -0300

    first commit

---
+++ src/main/java/com/github/nicolasvpagliari/gamelist/GamelistApplication.java
@@ -0,0 +1,13 @@
+package com.github.nicolasvpagliari.gamelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamelistApplication.class, args);
+	}
+
+}
